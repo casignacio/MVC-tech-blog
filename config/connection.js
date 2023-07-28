ed6e7194cffd641b3794688f0b84b9eb79c7e64f
casignacio <carloanthonyignacio@gmail.com>
2023-07-27 19:43:57 -0600

    debugging

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,27 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      // dialectOptions: {
-      //   decimalNumbers: true,
-      // },
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL){
+   sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    })
+}
+
+// const sequelize = process.env.JAWSDB_URL 
+//   ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//       // dialectOptions: {
+//       //   decimalNumbers: true,
+//       // },
+//     });
 
 module.exports = sequelize;
